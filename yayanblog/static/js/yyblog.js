26a9d716496c84727624fd02f89357ad66136743
wssyy1995 <18221091524@126.com>
2020-04-05 20:45:19 +0800

    add pictures change animation

--- yayanblog/static/js/yyblog.js
+++ yayanblog/static/js/yyblog.js
@@ -6,37 +6,39 @@ Gallery_recommend_big
     -点击4个小点切换
     -setInterval定时器自动轮播
     -上一张/下一张切换
+
+ （待优化：当前小点颜色切换使用class=active，再在css样式表中对active进行样式渲染，去颜色的话就删除这个class=active属性）
  */
 
 //定义全局变量和方法
 //children只返回节点下面的html元素节点，不返回一些文本节点
+
+
 
 //获取图片列表和小点列表对象
 var slide_trigger=document.getElementById('slide_trigger').children;
 var recommend_slide=document.getElementById('recommend_slide').children;
 var pre_pic_btn=document.getElementById('left_trigger');
 var nex_pic_btn=document.getElementById('right_trigger');
+
+
 //定义一个全局的轮播图（正在、希望）跳转的图和小点的索引
 var c_index=0;
 
 //定义一个更换图片的方法，接收参数为希望跳转的图片和小点的索引
 var change_picture=function(ind){
-    for(let i=0;i<recommend_slide.length;i++){
-            //for循环将图片list每个图片的z-index先恢复统一 z-index
-            recommend_slide[i].style['z-index']=100;
-        }
-
-        // 接收希望跳转的索引参数，将对应的图片z-index设为100以上
-        recommend_slide[ind].style['z-index']=101;
+        document.querySelector(".active_pic").className='recommend_big_item';
+        // 接收希望跳转的索引参数，将对应的图片对象的class值添加上active_pic
+        recommend_slide[ind].className+=' active_pic';
 
 }
 
 //定义一个更换小点颜色的方法，接收参数为希望跳转的图片和小点的索引
 var change_dot=function(ind){
-        for(let i=0;i<slide_trigger.length;i++){
-                slide_trigger[i].style['background-color']='white';
-            }
-        slide_trigger[ind].style['background-color']='green';
+        //将之前标记为active_dot的小点的的class值恢复成默认; 注意这里寻找active_dot的是会变的对象，不能将它赋值给一个变量；
+        document.querySelector(".active_dot").className='dot';
+        //把选中的小点对象的class值添加上active_dot
+        slide_trigger[ind].className+=' active_dot';
 
 
 }
@@ -55,7 +57,8 @@ for(let c=0;c<slide_trigger.length;c++){
     }
 
 }
-//setInterval定时器自动轮播
+
+// setInterval定时器自动轮播
 // 定义一个轮播图函数
 function autochange(){
         c_index++;
@@ -66,24 +69,10 @@ function autochange(){
         change_picture(c_index);
         change_dot(c_index);
 
-        // for(let i=0;i<recommend_slide.length;i++){
-        //     recommend_slide[i].style['z-index']=100;
-        // }
-        //
-        // recommend_slide[c_index].style['z-index']=110;
-        //
-        // for(let i=0;i<slide_trigger.length;i++){
-        //             slide_trigger[i].style['background-color']='white';
-        //         }
-        // slide_trigger[c_index].style['background-color']='green';
-
-
-
 }
 
-// 创建一个定时器开启计时，传入轮播图函数，每2.5秒就会执行这个函数
-var auto_picture=setInterval(autochange, 2500);
-
+// 创建一个定时器开启计时，传入轮播图函数，每2秒就会执行这个函数
+var auto_picture=setInterval(autochange, 3000);
 var gallery_recommend_big=document.getElementById('gallery_recommend_big');
 
 //鼠标移入的时候清除定时器
@@ -97,7 +86,7 @@ gallery_recommend_big.onmouseover=function(){
 //鼠标移出的时候重启定时器
 gallery_recommend_big.onmouseout=function(){
     console.log('timer restart');
-    auto_picture=setInterval(autochange, 2500);
+    auto_picture=setInterval(autochange, 3000);
 
 };
 
